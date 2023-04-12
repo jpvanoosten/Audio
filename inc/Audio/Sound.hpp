659dcc7e9ad24a2956da5530693bffbbd28984a5
Jeremiah van Oosten <jpvanoosten@hotmail.com>
2023-04-12 16:09:55 +0200

    Sound constructor defaults to Type::Sound.

--- inc/Audio/Sound.hpp
+++ inc/Audio/Sound.hpp
@@ -27,7 +27,7 @@ public:
         Exponential,  ///< Exponential attenuation. Equivalent to OpenAL's AL_EXPONENT_DISTANCE_CLAMPED.
     };
 
-    explicit Sound( const std::filesystem::path& filePath, Type type );
+    explicit Sound( const std::filesystem::path& filePath, Type type = Type::Sound );
 
     /// <summary>
     /// Load a sound effect from a file.

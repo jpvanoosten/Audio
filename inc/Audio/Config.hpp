2f4eee1c0715c6853e918e46786da11cc6da1535
Jeremiah van Oosten <jpvanoosten@hotmail.com>
2023-05-07 17:07:34 +0200

    Update config for WSL

--- inc/Audio/Config.hpp
+++ inc/Audio/Config.hpp
@@ -1,8 +1,14 @@
 #pragma once
 
-#if defined( _WIN32 )
+#if defined( _WIN32 ) || defined( __CYGWIN__ )
     #define AUDIO_IMPORT __declspec( dllimport )
     #define AUDIO_EXPORT __declspec( dllexport )
+#elseif defined(__GNUC__) && __GNUC__ >= 4
+    #define AUDIO_IMPORT __attribute__ ((visibility ("default")))
+    #define AUDIO_EXPORT __attribute__ ((visibility ("default")))
+#else
+    #define AUDIO_IMPORT
+    #define AUDIO_EXPORT
 #endif
 
 #if defined( Audio_EXPORTS )
